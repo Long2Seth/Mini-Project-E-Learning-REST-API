4cb5f0e53c4a173990ded11fb7319699a0ea6565
povsoknem <povsoknem@gmail.com>
2024-04-17 21:41:35 +0700

    Auto commit

--- src/main/java/co/istad/elearning/mapper/CourseMapper.java
+++ src/main/java/co/istad/elearning/mapper/CourseMapper.java
@@ -11,4 +11,5 @@ public interface CourseMapper {
     @Mapping(source = "", target = "course",
             qualifiedByName = "mapCourseResponse")
     CourseResponse toCourseResponse(Course course);
+
 }
